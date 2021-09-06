06e1084be97bfb76d195f5f7a2808eaf4bc2ff34
chalobon <chalobon-jaje@hotmail.com>
2021-09-06 17:43:21 +0700

    css layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 import Card from './components/Card';
 import { Title } from './components/Title';
+
+const buttonData = [
+  {
+    id: 1,
+    percent: 5,
+  },
+  {
+    id: 2,
+    percent: 10,
+  },
+  {
+    id: 3,
+    percent: 15,
+  },
+  {
+    id: 4,
+    percent: 25,
+  },
+  {
+    id: 5,
+    percent: 50,
+  },
+];
+
 function App() {
   return (
     <div className="main">
       <Title />
-      <Card />
+      <Card buttonData={buttonData} />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,123 @@
 import React from 'react';
+import { useState } from 'react';
+import dollar from '../images/icon-dollar.svg';
+import person from '../images/icon-person.svg';
 
-const Card = () => {
+const Card = ({ buttonData }) => {
+  const [percent, setPercent] = useState(0);
+  const [number, setNumber] = useState(1);
+  const [price, setPrice] = useState(0);
+  // const [tip_amount, setTip_amount] = useState(0);
+
+  const changePercent = (percent) => {
+    setPercent(percent);
+  };
+
+  const changePercentInput = (e) => {
+    const percent = e.target.value;
+    setPercent(percent);
+  };
+
+  const handleChangePrice = (e) => {
+    const value = e.target.value;
+    setPrice(value);
+  };
+
+  const handleChangeNumber = (e) => {
+    const value = e.target.value;
+    setNumber(value);
+  };
+
+  const handleReset = () => {
+    setPercent(0);
+    setNumber(1);
+    setPrice(0);
+  };
+
+  const tip_amount = (price * percent) / 100 / number;
+  const total = price / number + tip_amount;
+
   return (
     <div className="card">
       <section>
         <div className="cal">
           <div>
             <span>Bill</span>
-            <div class="input-box">
-              <span class="prefix">$</span>
-              <input type="number" placeholder="0" className="rightAlignText" />
+            <div className="input-box">
+              <span className="prefix">
+                <img src={dollar} alt="" />
+              </span>
+              <input
+                onChange={handleChangePrice}
+                value={price}
+                type="number"
+                placeholder="0"
+                className="rightAlignText"
+              />
             </div>
           </div>
 
           <div>
             <span>Select Tip %</span>
+            <div className="tip-percent">
+              {buttonData.map((item) => (
+                <button
+                  key={item.id}
+                  onClick={() => changePercent(item.percent)}
+                >
+                  {item.percent}%
+                </button>
+              ))}
+              <input
+                className="rightAlignText"
+                type="text"
+                placeholder="Custom"
+                onChange={changePercentInput}
+              />
+            </div>
+          </div>
+
+          <div>
+            <span>Number of People</span>
+            <div>
+              <div className="input-box">
+                <span className="prefix">
+                  <img src={person} alt="" />
+                </span>
+                <input
+                  onChange={handleChangeNumber}
+                  type="number"
+                  placeholder="0"
+                  className="rightAlignText"
+                />
+              </div>
+            </div>
           </div>
         </div>
 
-        <div className="result">right</div>
+        <div className="result">
+          <div>
+            <span>Tip Amount</span>
+            <p>/person</p>
+          </div>
+          <div className="summary">
+            <h1>${tip_amount}</h1>
+          </div>
+
+          <div>
+            <span>Total</span>
+            <p>/person</p>
+          </div>
+          <div className="summary">
+            <h1>${total}</h1>
+          </div>
+          <button
+            onClick={total === 0 ? '' : handleReset}
+            className={total === 0 ? 'reset-defalt' : 'reset'}
+          >
+            RESET
+          </button>
+        </div>
       </section>
     </div>
   );
